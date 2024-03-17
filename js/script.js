94db6b466e4ccf2c80b995d942a9649dd067a727
vika <viksavrub@gmail.com>
2024-03-17 23:08:19 +0200

    fix scripts

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 'use strict';
-document.addEventListener('click', documentActions);
-
-function documentActions(e) {
-    const targetElement = e.target;
-
-    if (targetElement.closest('.icon-menu')) {
-        document.body.classList.toggle('menu-open');
-    }
-
-    if (targetElement.closest('.form-submit')) {
+const submitFormButtonEl = document.querySelector('.form-submit');
+if (submitFormButtonEl instanceof HTMLElement) {
+    submitFormButtonEl.addEventListener('click', (event) => {
+        event.preventDefault();
         document.body.classList.add('show');
-    } else {
-        document.body.classList.remove('show');
-    }
-    // e.preventDefault();
+    });
+}
+
+
+const iconMenuButtonEl = document.querySelector('.icon-menu');
+if (iconMenuButtonEl instanceof HTMLElement) {
+    iconMenuButtonEl.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.body.classList.toggle('menu-open');
+    });
 }
